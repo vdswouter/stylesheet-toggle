c34c42758366f9ca475b1488d74f903143543495
Mike Henderyckx <mike.henderyckx@gmail.com>
2017-01-12 15:46:02 +0100

    WIP, toggle 3 stylesheets, order is not correct when togglin' yet

---
+++ script.js
@@ -0,0 +1,96 @@
+(function () {
+  'use strict';
+
+window.onload = function () {
+  var ui,
+    stylesheets,
+    styleObjects = [],
+    stylesheetUL;
+
+  function initialize() {
+    createUI();
+  }
+
+  // Helper Funtctions
+
+  function createUI() {
+    var div = document.createElement("div");
+    div.className = "my-ui";
+    div.style.minWidth = "150px";
+    div.style.padding = "4px";
+    div.style.position = "fixed";
+    div.style.bottom = "10px";
+    div.style.right = "10px";
+    div.style.background = "red";
+    div.style.color = "white";
+
+    // Text
+    div.innerHTML = "<h2>Stylesheets:</2><ul class=\"stylesheet-list\"><ul>";
+
+    document.querySelector("body").appendChild(div);
+
+    getCurrentStylesheets();
+  }
+
+  function getCurrentStylesheets() {
+    var s = document.querySelectorAll('link[rel=stylesheet]');
+    [].slice.call(s).forEach(function (stylesheet) {
+      styleObjects.push({ fileName: stylesheet.attributes.href.value, file: stylesheet, visible: true });
+    });
+
+    printCurrentStylesheets();
+  }
+
+  function printCurrentStylesheets() {
+    stylesheetUL = document.querySelector('.my-ui ul.stylesheet-list');
+    styleObjects.forEach(function (style) {
+      console.info(style);
+      var li = document.createElement("li");
+      li.innerHTML = style.fileName;
+      li.attributes.fileName = style.fileName;
+      li.attributes.file = style.file;
+      li.onclick = function (e) {
+        style.visible ? removeStyling(style) : addStyling(style)
+        style.visible = !style.visible;
+      };
+      stylesheetUL.appendChild(li)
+    });
+
+    resetMyUICss();
+  }
+
+  function removeStyling(style) {
+    document.head.removeChild(style.file);
+  }
+
+  function addStyling(style) {
+    document.head.appendChild(style.file);
+  }
+
+  function resetMyUICss() {
+    ui = document.querySelector('.my-ui');
+    [].slice.call(ui.getElementsByTagName("*"))
+      .forEach(function (element) {
+        element.style.margin = "0px";
+        element.style.padding = "0px";
+        element.style.color = "white";
+        element.style.fontSize = "14px";
+        element.style.textTransform = "none";
+        element.style.fontStyle = "normal";
+        element.style.border = "0";
+        element.style.listStyleType = "none";
+
+        if (element.tagName === "LI") {
+          element.style.cursor = "pointer";
+        }
+      });
+
+  }
+
+
+  // initialize
+
+  initialize();
+}
+
+})();
